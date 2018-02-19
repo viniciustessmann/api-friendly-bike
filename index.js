750957ab52194e4e8f162e65f77d6c2a5ef18ab1
Vinícius Schlee Tessmann <viniciusschleetessmann@gmail.com>
2018-02-19 10:06:02 -0300

    testing

--- index.js
+++ index.js
@@ -3,7 +3,3 @@ var http  = require('http');
 var server = http.createServer(function(req, res){
     res.end('Hello World');
 });
-
-server.listen(3000, function(){
-    console.log('Server run in http://localhost:3000');
-});
