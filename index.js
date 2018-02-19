fba1c2074fe435e9457cc11136163f31f4e823e9
Vinícius Schlee Tessmann <viniciusschleetessmann@gmail.com>
2018-02-19 10:15:41 -0300

    fix port

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var server = http.createServer(function(req, res){
     res.end('Hello World');
 });
 
-server.listen(process.env.PORT || 3000, function(){
-    console.log('Server run in http://localhost:3000');
-});
+var port_number = server.listen(process.env.PORT || 3000);
+
+// server.listen(process.env.PORT || 3000, function(){
+//     console.log('Server run in http://localhost:3000');
+// });
