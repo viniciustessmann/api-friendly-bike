0f53cf84c7e07d7c40222677c5d206f41847a624
Vinícius Schlee Tessmann <viniciusschleetessmann@gmail.com>
2018-02-19 10:08:17 -0300

    Fix port

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var http  = require('http');
 
 var server = http.createServer(function(req, res){
+
     res.end('Hello World');
+});
+
+server.listen(process.env.PORT || 3000, function(){
+    console.log('Server run in http://localhost:3000');
 });
