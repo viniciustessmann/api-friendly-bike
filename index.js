41af70254a93c6c57aca68318e256465732e68b6
Vinícius Schlee Tessmann <viniciusschleetessmann@gmail.com>
2018-02-19 10:19:28 -0300

    add env port

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var http  = require('http');
 
 var server = http.createServer(function(req, res){
-
     res.end('Hello World');
 });
 
-var port_number = server.listen(process.env.PORT || 3000);
+var port = process.env.PORT || 8000;
 
-// server.listen(process.env.PORT || 3000, function(){
-//     console.log('Server run in http://localhost:3000');
-// });
+server.listen(port, function() {
+    console.log("App is running on port " + port);
+});
